d8afe8dce954ca1f73d7c79155c172bc69b629db
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-19 17:36:52 +0200

    Fix

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ const corsOptions = {
 
 console.log('corsOptions', corsOptions)
 
-app.use(cors(corsOptions));
-
 app.use(express.json());
-app.use('/content', createProxyMiddleware({ target: services.content, changeOrigin: true }));
+app.use('/content', cors(corsOptions), createProxyMiddleware({ target: services.content, changeOrigin: true }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
