1b42b91153e05be2eb6f73e8e5f03d9390ae3eb1
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-19 16:52:34 +0200

    Proxy Server

--- index.js
+++ index.js
@@ -4,41 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const cors = require('cors')
-const axios = require('axios')
 const CONTENT_SERVICE_URL = process.env.CONTENT_SERVICE;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const services = {
+    content: CONTENT_SERVICE_URL
+}
+
+app.use(cors());
+app.use(express.json());
+app.use('/content', createProxyMiddleware({ target: services.content, changeOrigin: true }));
+
 const PORT = process.env.PORT || 3000;
-
-const app = express()
-
-app.use(cors({
-    origin: `${FRONTEND_URL}`
-}))
-
-app.use(express.json())
-
-app.all('/content/*', async (req, res) => {
-    let rocsContentServiceUrl = `${CONTENT_SERVICE_URL}${req.originalUrl}`;
-    console.log(rocsContentServiceUrl);
-    try {
-        const response = await axios({
-            method: req.method,
-            url: rocsContentServiceUrl,
-            data: req.body,
-            headers: {
-                accept: 'application/json',
-                ...req.headers,
-            }
-        });
-        res.status(response.status).send(response.data);
-    } catch (e) {
-        console.log(e.response.status);
-        console.log(e.response.data);
-        res.status(e.response.status).send(e.response.data);
-    }
-});
-
-
 app.listen(PORT, () => {
     console.log(`Gateway listening on port ${PORT}`)
 })
