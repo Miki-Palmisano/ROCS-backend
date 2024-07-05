6274a603acdeee752637d8ef317ea9e58050654b
Miki-Palmisano <palmmiki21@gmail.com>
2024-07-05 18:26:21 +0200

    Account Update

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const cors = require('cors')
-const CONTENT_SERVICE_URL = process.env.CONTENT_SERVICE;
 const axios = require('axios')
 const collectionFilm = require('./routes/films')
 const collectionSerie = require('./routes/series')
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const app = express();
 
 const services = {
-    content: CONTENT_SERVICE_URL
+    content: process.env.CONTENT_SERVICE,
+    database: process.env.DATABASE_SERVICE
 }
  
 const corsOptions = {
@@ -30,7 +34,7 @@ app.use('/content/films', collectionFilm)
 app.use('/content/series', collectionSerie)
 
 app.use('/content', (req, res) => {
-    const url = `${CONTENT_SERVICE_URL}${req.originalUrl.replace('/content', '')}`;
+    const url = `${services.content}${req.originalUrl.replace('/content', '')}`;
 
     axios({
         method: req.method,
@@ -44,6 +48,50 @@ app.use('/content', (req, res) => {
         console.error('Errore nel inoltrare la richiesta:', error);
         res.status(500).json({ error: 'Errore nel inoltrare la richiesta' });
     });
+});
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+};
+
+passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    done(null, jwt_payload); // In a real app, you might want to fetch the user from the database
+}));
+
+app.use(passport.initialize());
+
+const generateJWT = (user) => {
+    const payload = {
+        id: user.id,
+        username: user.username
+    };
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+app.post('/database/user/register', async (req, res) => {
+    try {
+        const response = await axios.post(`${services.database}/user/register`, req.body);
+        res.json(response.data);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+});
+
+app.post('/database/user/login', async (req, res) => {
+    try {
+        const response = await axios.post(`${services.database}/user/login`, req.body);
+        const user = response.data;
+        const token = generateJWT(user);
+        res.json({ token: token , username: user.username});
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.send('This is a protected route');
 });
 
 const PORT = process.env.PORT || 3000;
